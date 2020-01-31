e7930db22a5a716ae747286a185298f078ef52aa
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-31 15:18:46 +0100

    inserimento ajax e template

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
   console.log('hello');
+  var url = "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
+  $.ajax({
+    url: url,
+    method: "GET",
+    success: function (data, stato) {
+    $("#risultati").html(data);
+    },
+    error: function (richiesta, stato, errori) {
+    alert("E' avvenuto un errore. " + errore);
+    }
+  });
 
 });
